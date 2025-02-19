058fdbed3fc4f6fb72e66b026dac7d003f3265f6
infoalitech <asad07538@gmail.com>
2025-02-19 13:44:21 +0500

    blogDetail

--- public/views/blogDetail.php
+++ public/views/blogDetail.php
@@ -37,17 +37,3 @@ include 'snippets/header.php';
 
 
 <?php include 'snippets/footer.php'; ?>
-
-
-<?php include 'snippets/footer.php'; ?>
-
-
-<div class="container mt-4">
-    <h1 class="text-center"><?= htmlspecialchars($blog['title']) ?></h1>
-    <img src="<?= $basePath ?>/<?= $blog['cover'] ?>" class="img-fluid my-3" alt="<?= htmlspecialchars($blog['title']) ?>">
-    
-    <p><strong>Published on:</strong> <?= htmlspecialchars($blog['published_date']) ?></p>
-    <p><?= nl2br(htmlspecialchars($blog['description'])) ?></p>
-</div>
-
-<?php include 'snippets/footer.php'; ?>
